c24b833faa7fb53093386015cd4ca5c01067dfd7
Luisttine <luisttine@gmail.com>
2021-03-24 18:22:31 -0300

    Pega esse Jony

--- scripts/user.js
+++ scripts/user.js
@@ -1,4 +1,4 @@
 function teste() {
     var name = window.prompt("Enter your name: "); window="contato"; 
-    document.querySelector("[name='teste1']").value = informacao;
+    document.querySelector("[name='teste1']").value = "contato";
 }
